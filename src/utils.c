cefb4c92cb4b2151c68a1d9f06451971f015bdbe
James Poole <jgaple@gmail.com>
2025-07-31 15:41:12 +0200

    Scaling text position

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,18 @@
 #include "utils.h"
+#include "raymath.h"
+
+// TODO: This is v rough. Tidy up and clarify naming
+Vector2 ScalePointBasedOnRef(int startWidth, Vector2 pointToScale) {
+  int screen_width = GetScreenWidth();
+
+  int padding = GetWindowPadding();
+
+  int desired_width = screen_width - padding;
+
+  int ratio = desired_width / startWidth;
+
+  return Vector2Scale(pointToScale, ratio);
+}
 
 Vector2 GetScaledUpDimensions(int start_width, int start_height) {
   int screen_width = GetScreenWidth();
--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,7 @@
 #include "raylib.h"
 typedef enum { INVISIBLE, ENTER, VISIBLE, EXIT, DONE } ShowState;
 
+Vector2 ScalePointBasedOnRef(int startWidth, Vector2 pointToScale);
 Vector2 GetScaledUpDimensions(int original_width, int original_height);
 int GetWindowPadding(void);
 
